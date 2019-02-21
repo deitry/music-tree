847986ba3f52805ad1681e20f0c945841bd5b943
Dmitriy Vornychev <dm.s.vornychev@gmail.com>
2019-02-22 02:04:41 +0300

    Работающий пример воспроизведения аудио

--- music_tree/player.py
+++ music_tree/player.py
@@ -1,9 +1,18 @@
 """ Принимает на вход ноду, раскрывает её и проигрывает содержимое """
 
-class Player():
+import math
+from pyaudio import PyAudio
+from datetime import datetime, timedelta
 
+
+class Player():
     def __init__(self):
         self.stopped = True
+        self.last = datetime.now()
+
+        self.bitrate = 16000
+        self.p = PyAudio()
+        self.stream = None
 
     def play(self, node):
 
@@ -24,3 +33,52 @@ class Player():
                 # instrument.play(notes)
                 notes = instrument.getNotes(node, pos)
                 instrument.play(notes)
+
+    def testPlay(self, note, noteLen):
+
+        if self.stream == None:
+            raise AssertionError("Must be called in context manager")
+
+        noteHz = 440 * pow(2., note / 12.)
+
+        FREQUENCY = noteHz
+        LENGTH = noteLen
+
+        NUMBEROFFRAMES = int(self.bitrate * LENGTH)
+
+        WAVEDATA = ''
+
+        MAX = 8
+        MAX_2 = int(MAX / 2)
+
+        CENTER = MAX_2
+
+        for x in range(NUMBEROFFRAMES):
+            WAVEDATA += chr(
+                int(
+                    math.sin(x / ((self.bitrate / FREQUENCY) / math.pi)) *
+                    (MAX_2 - 1) + CENTER))
+
+        time = datetime.now()
+        delta = timedelta(seconds=noteLen)
+
+        # для отладки задержек
+        # print("times: ", time, self.last, delta)
+        # len(WAVEDATA) / BITRATE,
+        # print("len: ",  time - self.last - delta)
+
+        self.last = time
+        self.stream.write(WAVEDATA)
+
+    def __enter__(self):
+        self.stream = self.p.open(
+            format=self.p.get_format_from_width(1),
+            channels=1,
+            rate=self.bitrate,
+            output=True)
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.stream.stop_stream()
+        self.stream.close()
+        self.p.terminate()
--- tests/test_player.py
+++ tests/test_player.py
@@ -15,3 +15,40 @@ def test_01_play_node():
         # проверять количество вызовов SoundEngine на соответствие нотам
     except:
         pytest.fail("player did not make it...")
+
+@pytest.mark.sound
+def test_02_testPlay():
+    try:
+        with Player() as pl:
+            pl.testPlay(0, 0.2)
+            pl.testPlay(15, 0.2)
+            pl.testPlay(14, 0.2)
+            pl.testPlay(10, 0.2)
+            pl.testPlay(12, 0.8)
+            pl.testPlay(14, 0.4)
+            pl.testPlay(7, 0.4)
+            pl.testPlay(10, 0.8)
+
+            pl.testPlay(12, 0.27)
+            pl.testPlay(13, 0.27)
+            pl.testPlay(12, 0.27)
+            pl.testPlay(0, 0.8)
+    except AttributeError:
+        print(1)
+
+        # player.testPlay(12, 1)
+        # player.testPlay(0, 1)
+        # player.testPlay(12, 1)
+        # player.testPlay(0, 1)
+        # player.testPlay(12, 1)
+        # player.testPlay(0, 1)
+
+
+        # player.testPlay(12, 0.125)
+        # player.testPlay(12, 0.125)
+        # player.testPlay(12, 0.125)
+        # player.testPlay(12, 0.125)
+        # player.testPlay(0, 0.125)
+        # player.testPlay(0, 0.125)
+        # player.testPlay(0, 0.125)
+        # player.testPlay(0, 0.125)
