c32182d4601fa1c418e499cc2f2c93ed25fad66c
vornichev <vornichev@elins.ru>
2019-03-26 19:07:45 +0300

    Started work on structure elements

---
+++ tests/test_structure.py
@@ -0,0 +1,19 @@
+# import pytest
+from music_tree.base.composition import Symbol, Word, Text
+
+
+def test_word():
+
+    word = Word()
+    word.symbols.append(Symbol(1))
+    word.symbols.append(Symbol(2))
+    word.symbols.append(Symbol(3))
+    word.symbols.append(Symbol(1))
+
+    # для простоты
+    word.symbols[0].accent = True
+    word.symbols[2].accent = True
+
+    text = Text()
+
+    # TODO: вставка слова в текст с привязкой к конкретным snappoints
